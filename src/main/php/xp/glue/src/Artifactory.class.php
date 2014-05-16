57e9291a1e48ab9d5420d03e32fd5493714c6c1a
Timm Friebe <thekid@thekid.de>
2014-05-16 15:09:17 +0200

    Parse pom.xml - prepare dependencies

--- src/main/php/xp/glue/src/Artifactory.class.php
+++ src/main/php/xp/glue/src/Artifactory.class.php
@@ -5,6 +5,7 @@ use webservices\rest\RestRequest;
 use peer\http\HttpConnection;
 use peer\URL;
 use xp\glue\Download;
+use xp\glue\input\MavenPOM;
 
 /**
  * Artifactory source
@@ -17,6 +18,10 @@ class Artifactory extends Source {
     // $this->rest->setTrace((new \util\log\LogCategory(''))->withAppender(new \util\log\ConsoleAppender()));
   }
 
+  public function relativeUri(URL $base, $uri) {
+    return substr((new URL($uri))->getPath(), strlen($base->getPath()));
+  }
+
   public function fetch($vendor, $name, $version) {
     $res= $this->rest->execute((new RestRequest('/api/search/gavc'))
       ->withParameter('g', $vendor)
@@ -27,20 +32,25 @@ class Artifactory extends Source {
 
     if (200 === $res->status()) {
       $response= $res->data();
+      $base= $this->rest->getBase();
       foreach ($response['results'] as $result) {
-        if (strstr($result['uri'], '.xar')) {
-          $base= $this->rest->getBase();
+        $info= $this->rest->execute((new RestRequest($this->relativeUri($base, $result['uri'])))
+          ->withAccept('application/vnd.org.jfrog.artifactory.storage.FileInfo+json')
+        )->data();
 
-          $relative= substr((new URL($result['uri']))->getPath(), strlen($base->getPath()));
-          $info= $this->rest->execute((new RestRequest($relative))
-            ->withAccept('application/vnd.org.jfrog.artifactory.storage.FileInfo+json')
-          )->data();
+        $uri= new URL($info['downloadUri']);
+        if ($uri->getHost() === $base->getHost()) {
+          $uri->setUser($base->getUser());
+          $uri->setPassword($base->getPassword());
+        }
 
-          $uri= new URL($info['downloadUri']);
-          if ($uri->getHost() === $base->getHost()) {
-            $uri->setUser($base->getUser());
-            $uri->setPassword($base->getPassword());
-          }
+        if (strstr($result['uri'], '.pom')) {
+          $pom= (new HttpConnection($uri))->get();
+
+          $project= (new MavenPOM())->parse($pom->getInputStream());
+          var_dump($project);  // XXX TODO 
+
+        } else if (strstr($result['uri'], '.xar')) {
           $file= (new HttpConnection($uri))->get();
           if (200 === $file->statusCode()) return new Download(
             $file->getInputStream(),
