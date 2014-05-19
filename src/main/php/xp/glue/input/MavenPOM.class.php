25a1784c90c71fd5ddeb77cacd1802055516cee3
Timm Friebe <thekid@thekid.de>
2014-05-19 11:19:23 +0200

    Support POM variables such as `${project.version}`

--- src/main/php/xp/glue/input/MavenPOM.class.php
+++ src/main/php/xp/glue/input/MavenPOM.class.php
@@ -30,25 +30,37 @@ class MavenPOM extends \lang\Object {
     $tree= new Tree();
     $this->parser->withCallback($tree)->parse(new StreamInputSource($in, $source));
 
-    $pom= new XPath($tree);
-    $pom->context->registerNamespace('pom', 'http://maven.apache.org/POM/4.0.0');
-    $textOf= function($node, $child) use($pom) { return $pom->query($child, $node)->item(0)->textContent; };
+    try {
+      $pom= new XPath($tree);
+      $pom->context->registerNamespace('pom', 'http://maven.apache.org/POM/4.0.0');
+      $resolve= function($matches) use($pom) {
+        $query= '/'.implode('/', array_map(function($e) { return 'pom:'.$e; }, explode('.', $matches[1])));
+        return $pom->query($query)->item(0)->textContent;
+      };
+      $textOf= function($node, $child) use($pom, $resolve) {
+        $item= $pom->query($child, $node)->item(0);
+        return $item ? preg_replace_callback('/\$\{([^\}]+)\}/', $resolve, $item->textContent) : null;
+      };
 
-    $dependencies= [];
-    foreach ($pom->query('/pom:project/pom:dependencies/pom:dependency') as $dep) {
-      $classifier= $textOf($dep, 'pom:classifier');
-      $dependencies[]= new Dependency(
-        $textOf($dep, 'pom:groupId'),
-        $textOf($dep, 'pom:artifactId').($classifier ? '-'.$classifier : ''),
-        new Requirement($textOf($dep, 'pom:version'))
+      $dependencies= [];
+      foreach ($pom->query('/pom:project/pom:dependencies/pom:dependency') as $dep) {
+        $classifier= $textOf($dep, 'pom:classifier');
+        $dependencies[]= new Dependency(
+          $textOf($dep, 'pom:groupId'),
+          $textOf($dep, 'pom:artifactId').($classifier ? '-'.$classifier : ''),
+          new Requirement($textOf($dep, 'pom:version'))
+        );
+      }
+
+      return new Project(
+        $textOf($pom->document, 'pom:groupId'),
+        $textOf($pom->document, 'pom:artifactId'),
+        strtr($textOf($pom->document, 'pom:version'), ['-SNAPSHOT' => '']),
+        $dependencies
       );
+    } catch (\lang\XPException $e) {
+      \util\cmd\Console::writeLine($tree->getSource());
+      throw new \lang\FormatException('Errors processing '.$source, $e);
     }
-
-    return new Project(
-      $textOf($pom->document, 'pom:groupId'),
-      $textOf($pom->document, 'pom:artifactId'),
-      strtr($textOf($pom->document, 'pom:version'), ['-SNAPSHOT' => '']),
-      $dependencies
-    );
   }
 }
